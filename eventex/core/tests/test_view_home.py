c657e664a5c24cdeb02e77d47a1ebc8a12f16b04
Cleber Fonseca <cleber@3wsolution.com.br>
2016-01-27 02:37:48 -0200

    Talk

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -40,3 +40,8 @@ class HomeTest(TestCase):
         expected = 'href="{}#speakers"'.format(r('home'))
         self.assertContains(self.response, expected)
 
+
+    def test_talks_link(self):
+        expected = 'href="{}"'.format(r('talk_list'))
+        self.assertContains(self.response, expected)
+
